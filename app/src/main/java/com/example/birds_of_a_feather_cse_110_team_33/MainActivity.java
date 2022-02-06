c25dfe7ce2909b0641bb27205ad5a7970653070f
Jeremiah Mao <jeremiahmao@Jeremiahs-MacBook-Air.local>
2022-02-06 03:56:22 -0800

    New Android Studio Project created with the default gitignore file

---
+++ app/src/main/java/com/example/birds_of_a_feather_cse_110_team_33/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.birds_of_a_feather_cse_110_team_33;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
