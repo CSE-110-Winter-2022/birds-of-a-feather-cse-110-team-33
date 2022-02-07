e45f064cb5265e198e2b251e1d3ee74224501079
CMoore6258 <78571486+CMoore6258@users.noreply.github.com>
2022-02-07 02:25:48 -0800

    Android Test Testing2

---
+++ app/src/test/java/com/example/birds_of_a_feather_cse_110_team_33/UserStoryOneTestDefaultValueUnitTest.java
@@ -0,0 +1,70 @@
+package com.example.birds_of_a_feather_cse_110_team_33;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.runner.AndroidJUnit4;
+import static org.junit.Assert.*;
+
+import com.example.birds_of_a_feather_cse_110_team_33.model.AddAClassPage;
+import com.example.birds_of_a_feather_cse_110_team_33.model.UserClassList;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class UserStoryOneTestDefaultValueUnitTest {
+
+    @Rule
+    public ActivityScenarioRule<UserClassList> scenarioRule = new ActivityScenarioRule<>(UserClassList.class);
+    public ActivityScenarioRule<AddAClassPage> scenarioRule2 = new ActivityScenarioRule<>(AddAClassPage.class);
+
+
+    @Test
+    public void testDefaultValue() {
+
+        ActivityScenario<UserClassList> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+
+            TextView resultView = activity.findViewById(R.id.course_edit_text);
+
+            String extractedText = resultView.getText().toString();
+            String guess = "CSE 110 Winter 2022";
+
+
+            assertEquals(extractedText, guess);
+        });
+    }
+
+    @Test
+    public void testChangingText() {
+
+        ActivityScenario<AddAClassPage> scenario2 = scenarioRule2.getScenario();
+
+        scenario2.moveToState(Lifecycle.State.CREATED);
+
+        scenario2.onActivity(activity -> {
+
+            EditText resultView1 = activity.findViewById(R.id.course_subject_edit_text);
+            String extractedHint = resultView1.getHint().toString();
+
+            String guess = "CSE";
+
+            assertEquals(extractedHint, guess);
+        });
+    }
+
+
+
+
+}
+
