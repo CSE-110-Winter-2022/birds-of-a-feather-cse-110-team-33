1e7635defc2aefdc568392f909d402a699f5ea1d
e5chang <e5chang@ucsd.edu>
2022-02-13 21:55:44 -0800

    espresso

--- app/src/main/java/com/example/birds_of_a_feather_cse_110_team_33/UserClassListActivity.java
+++ app/src/main/java/com/example/birds_of_a_feather_cse_110_team_33/UserClassListActivity.java
@@ -59,8 +59,14 @@ public class UserClassListActivity extends AppCompatActivity {
 
     public void loadNewClasses() {
 
+        LinearLayout firstEntry = (LinearLayout) parentLinearLayout.getChildAt(0);
+        TextView firstTextBox = (TextView) firstEntry.getChildAt(0);
+        if (firstTextBox.getText().toString().equals("Example")) {
+            parentLinearLayout.removeView((View) firstTextBox.getParent());
+        }
+
         SharedPreferences preferences = getSharedPreferences("pref one",MODE_PRIVATE);
-        for (int i = 1; i < howManyCreatedThusFar; i++) {
+        for (int i = 0; i < howManyCreatedThusFar; i++) {
 
             addRow();
 
@@ -122,10 +128,6 @@ public class UserClassListActivity extends AppCompatActivity {
         intent.putExtra("quarterSpinnerChoice",quarterSpinnerChoice);
         intent.putExtra("yearSpinnerChoice",yearSpinnerChoice);
         startActivity(intent);
-    }
-
-    public void onGoBackClickedLD(View view) {
-        finish();
     }
 
     public void onFinishedClicked(View view) {
