dcd948256ea12c1819c88645c38236f4f19bf987
CMoore6258 <78571486+CMoore6258@users.noreply.github.com>
2022-03-11 00:41:46 -0800

    Fixed Testing Issues : Favorites_feature

--- app/src/test/java/com/example/birds_of_a_feather_cse_110_team_33/FavoritesTest.java
+++ app/src/test/java/com/example/birds_of_a_feather_cse_110_team_33/FavoritesTest.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.test.core.app.ApplicationProvider;
 
 import com.example.birds_of_a_feather_cse_110_team_33.model.db.AppDatabase;
+import com.example.birds_of_a_feather_cse_110_team_33.model.db.Course;
+import com.example.birds_of_a_feather_cse_110_team_33.model.db.Person;
 import com.example.birds_of_a_feather_cse_110_team_33.model.db.PersonDao;
 
 import org.junit.After;
@@ -20,6 +22,7 @@ import org.robolectric.*;
 public class FavoritesTest {
     private PersonDao personDao;
     private AppDatabase db;
+    private Person ethan;
 
     @Before
     public void setupTestDb() {
@@ -27,6 +30,20 @@ public class FavoritesTest {
         AppDatabase.useTestSingleton(context);
         db = AppDatabase.singleton(context);
         personDao = db.personDao();
+
+
+        ethan = new Person("Ethan", "https://i.kym-cdn.com/photos/images/original/001/431/201/40f.png");
+        ethan.setPersonId(0);
+        personDao.insert(ethan);
+        Course ethan110 = new Course(ethan.getPersonId(), 2022, "Winter", "CSE", "110", "Tiny");
+        Course ethan112 = new Course(ethan.getPersonId(), 2022, "Winter", "CSE", "112", "Small");
+        Course ethan132A = new Course(ethan.getPersonId(), 2022, "Spring", "CSE", "132A", "Large");
+        db.coursesDao().insert(ethan110);
+        db.coursesDao().insert(ethan112);
+        db.coursesDao().insert(ethan132A);
+
+        ethan.setNFavorite();
+
     }
 
     @After
@@ -38,9 +55,12 @@ public class FavoritesTest {
     public void testSavingFavorites() {
         PersonDetailActivity activity = Robolectric.setupActivity(PersonDetailActivity.class);
 
+
+
+
         activity.findViewById(R.id.favorite).performClick();
 
-        assertEquals(true, personDao.get(1).isFavorite);
+        assertEquals(true, personDao.get(0).isFavorite);
     }
 
     @Test
